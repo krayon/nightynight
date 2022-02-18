60c9aeefadbe5f43ed7a1ad4261be838f818669f
Krayon <krayon.git@qdnx.org>
2022-02-18 23:21:37 +1100

    Webserver hacking

--- web_server.py
+++ web_server.py
@@ -70,12 +70,146 @@ def serve(reader, writer):
     finally:
         yield from writer.aclose()
 
-def start(): #{
-    import logging
-    logging.basicConfig(level=logging.ERROR)
+@asyncio.coroutine
+def MY_serve(reader, writer): #{
+    print("serve()");
+    data = '';
+    while 1: #{
+        print("while 1");
+        more = (yield from reader.read());
+        more = more.decode() if more is not None else '';
+        print(more);
+        if (len(more) < 1): #{
+            print("NO MORE DATA");
+            break;
+        #}
 
-    loop = asyncio.get_event_loop()
-    loop.call_soon(asyncio.start_server(serve, "0.0.0.0", 80, 20))
-    loop.run_forever()
-    loop.close()
+        print("more: " + more);
+        data += more;
+    #}
+    print(data)
+
+    try:
+        file = decode_path((yield from reader.read()))
+        print("Serving file: " + file);
+        if exists(file): #{
+            mime_type, cacheable = get_mime_type(file)
+            if file.endswith(".py"): #{
+                mod = None;
+                method = 'POST'; # FIXME: REMOVE THIS TEMP
+
+                vardict = {}; # FIXME: REMOVE THIS TEMP
+                if (not vardict): #{
+                    vardict = {};
+                #}
+
+                try: #{
+                    modpath = file.rsplit(".", 1)[0];
+                    modname = modpath.rsplit("/", 1)[1];
+                    mod     = __import__(modpath);
+                    print("modpath: " + modpath);
+                    print("modname: " + modname);
+
+                except: #}{
+                    yield from writer.awrite("HTTP/1.0 500 Exec1\r\n\r\n");
+                    yield from writer.aclose();
+                    return;
+                #}
+
+                try: #{
+                    #modfunc = getattr(mod, modname);
+                    modfunc = getattr(mod, method);
+
+                except: #}{
+                    yield from writer.awrite("HTTP/1.0 500 Exec2\r\n\r\n");
+                    yield from writer.aclose();
+                    return;
+                #}
+
+                #try: #{
+                #    # "Execute" the python module (or more specifically, it's
+                #    # function of the same name) ... UPDATE: Now use a function
+                #    # named the method (eg. GET, POST etc)
+                #    #
+                #    # NOTE: You MUST output Content-Type and newline yourself
+                #    buffer = modfunc(vardict);
+
+                #    yield from writer.awrite("HTTP/1.0 200 OK\r\n");
+                #    #yield from writer.awrite("Content-Type: {}\r\n".format(mime_type));
+                #    #yield from writer.awrite("\r\n");
+                #    yield from writer.awrite(buffer);
+
+                #except: #}{
+                #    yield from writer.awrite("HTTP/1.0 500 Exec3\r\n\r\n");
+                #    yield from writer.aclose();
+                #    return;
+                ##}
+
+                buffer = modfunc(vardict);
+                ####yield from writer.awrite("HTTP/1.0 200 OK\r\n");
+
+                ##### FIXME: if buffer comes back as nothing (such as if I
+                ##### async.sleep in the func) then write.awrite fails as buffer
+                ##### is of type 'generator' and doesn't have .len()
+
+                ###yield from writer.awrite(buffer);
+                ##if (buffer): yield from writer.awrite(buffer);
+                #if (buffer is not None): yield from writer.awrite(buffer);
+                yield from writer.awrite(buffer);
+
+                try: #{
+                    modfunc = getattr(mod, 'end' + method);
+
+                except: #}{
+                    yield from writer.aclose();
+                    return;
+                #}
+
+                buffer = modfunc(vardict);
+
+                ##### FIXME: if buffer comes back as nothing (such as if I
+                ##### async.sleep in the func) then write.awrite fails as buffer
+                ##### is of type 'generator' and doesn't have .len()
+
+                yield from writer.awrite(buffer);
+
+            else: #}{
+                # Headers
+                yield from writer.awrite("HTTP/1.0 200 OK\r\n");
+                yield from writer.awrite("Content-Type: {}\r\n".format(mime_type));
+                if cacheable: #{
+                    yield from writer.awrite("Cache-Control: max-age=86400\r\n");
+                #}
+                yield from writer.awrite("\r\n");
+
+                f = open(file, "rb");
+                buffer = f.read(size_buffer);
+                while buffer != b'': #{
+                    yield from writer.awrite(buffer);
+                    buffer = f.read(size_buffer);
+                #}
+                f.close();
+            #}
+        else: #}{
+            yield from writer.awrite("HTTP/1.0 404 NA\r\n\r\n")
+        #}
+    except:
+        raise
+    finally:
+        yield from writer.aclose()
 #}
+
+def start(): #{
+    import logging;
+    logging.basicConfig(level=logging.ERROR);
+
+    print("Initialising event loop...");
+    loop = asyncio.get_event_loop();
+    print("Starting server...");
+    loop.call_soon(asyncio.start_server(serve, "0.0.0.0", 80, 20));
+    print("Entering loop...");
+    loop.run_forever();
+    loop.close();
+#}
+
+# vim:ts=4:tw=80:sw=4:et:ai:si
