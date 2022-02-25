c1ff85cdccf928ba0cc5d216eb0c41a1bbda751a
Krayon <krayon.git@qdnx.org>
2022-02-26 03:03:07 +1100

    pyhtml functionality

--- index.py
+++ index.py
@@ -1,40 +1,38 @@
 import os;
 import globs;
 
-# Returns:
-#   <s:buffer>, <i:next_offset>
-def index(size_buf, offset, vardict, body): #{
-    print("index.index(size_buf:%d, offset:%d, vardict, body)" % (size_buf, offset));
-    return "", -1;
-#}
-
-# Returns:
-#   <s:buffer>
-def __DEFAULT_PAGE(title, ver, uid, mac): #{
-    return """<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>""" + title + """</title><link rel="stylesheet" type="text/css" href="web_nn.css"></head><body><div class="mainTables"><table class="mainTables"><tr><td rowspan="6">&nbsp;</td><td colspan="2">""" + title + """</td><td rowspan="6">&nbsp;</td></tr><tr><td>Device Unique ID:</td><td class="ro"><pre>""" + uid + """</pre></td></tr><tr><td>MAC Address:</td><td class="ro"><pre>""" + mac + """</pre></td></tr><tr><td colspan=2>&nbsp;</td></tr><tr><td colspan=2><a href="web_reboot.py" >Reboot</a></td></tr><tr><td colspan=2>&nbsp;</td></tr><tr><td class="byline ro" colspan=4><em>""" + ver + """</em></td></tr></table></div></body></html>""";
-#}
+from web_pyhtml import pyhtml_parser;
 
 # Returns:
 #   <s:buffer>, <i:next_offset>
 def GET(size_buf, offset, vardict, body): #{
     print("index.GET(size_buf:%d, offset:%d, vardict, body)" % (size_buf, offset));
 
+    buf    = '';
+    read   =  0;
+
+    # Variables
     title = globs.prod + " " + globs.desc;
     ver = '<a href="' + globs.url + '">' \
         + title + " " + globs.ver + "<br>\n" \
         + "on " + os.uname().machine + " (" + os.uname().sysname + ") " + "<br>\n" \
         + "uPython " + os.uname().version + " (" + os.uname().release + ")" \
         + '</a>';
+    uid = globs.uid;
+    mac = globs.mac;
 
-    return "HTTP/1.0 200 OK\n" + "Content-Type: text/html\n\n" + __DEFAULT_PAGE(title, ver, globs.uid, globs.mac), -1;
+    tag, buf, read = pyhtml_parser('index.pyhtml', size_buf, offset, vardict, body);
+    if (tag): #{
+        print("Processing py tag:|" + buf + "|");
+        buf = eval(buf, {
+             'title': title
+            ,'ver':   ver
+            ,'uid':   uid
+            ,'mac':   mac
+        });
+    #}
+
+    return buf, read;
 #}
-
-### # Returns:
-### #   <s:buffer>, <i:next_offset>
-### def endGET(size_buf, offset, vardict, body): #{
-###     print("index.endGET()");
-###
-###     return "HTTP/1.0 200 OK\nContent-Type: text/html\n\n", -1;
-### #}
 
 # vim:ts=4:tw=80:sw=4:et:ai:si
--- web_reboot.py
+++ web_reboot.py
@@ -1,12 +1,7 @@
 import os;
 import globs;
 
-# Returns:
-#   <s:buffer>, <i:next_offset>
-def web_reboot(size_buf, offset, vardict, body): #{
-    print("web_reboot.web_reboot(size_buf:%d, offset:%d, vardict, body)" % (size_buf, offset));
-    return "", -1;
-#}
+from web_pyhtml import pyhtml_parser;
 
 # Returns:
 #   <s:buffer>, <i:next_offset>
@@ -16,66 +11,28 @@ def GET(size_buf, offset, vardict, body): #{
     buf    = '';
     read   =  0;
 
+    # Variables
     title = globs.prod + " " + globs.desc;
+    ver = '<a href="' + globs.url + '">' \
+        + title + " " + globs.ver + "<br>\n" \
+        + "on " + os.uname().machine + " (" + os.uname().sysname + ") " + "<br>\n" \
+        + "uPython " + os.uname().version + " (" + os.uname().release + ")" \
+        + '</a>';
+    uid = globs.uid;
+    mac = globs.mac;
 
-    try: #{
-        f = open('web_reboot.pyhtml', 'rb');
-    except: #}{
-        print('[ERR ] Template not found');
-        return "HTTP/1.0 404 Not Found\n\n", -1;
+    tag, buf, read = pyhtml_parser('web_reboot.pyhtml', size_buf, offset, vardict, body);
+    if (tag): #{
+        print("Processing py tag:|" + buf + "|");
+        buf = eval(buf, {
+             'title': title
+            ,'ver':   ver
+            ,'uid':   uid
+            ,'mac':   mac
+        });
     #}
 
-    output = '';
-    if (offset <= 0): #{
-        output = "HTTP/1.0 200 OK\nContent-Type: text/html\n\n";
-    else: #}{
-        f.seek(offset);
-    #}
-
-    # TODO: Handle if we only get half a encoded tag better ("<?py ... ?>")
-    #       For now, we skip the tag :facepalm:
-
-    code_s = 99;
-    code_e = -1;
-    while (code_s >= 0 and code_e < 0): #{
-        offset = offset + read;
-        buf = f.read(size_buf - len(output));
-        buf = buf.decode();
-        read = len(buf);
-        if (read == 0): #{
-            # We're done
-            f.close(); f = None;
-            return output, -1;
-        #}
-        code_s = buf.find('<?py');
-        code_e = buf.find('?>');
-    #}
-
-    # If there's no "<?py", just put it straight through
-    if (code_s < 0): #{
-        f.close(); f = None;
-        return output + buf, offset + read;
-    #}
-
-    # If there's a "<?py" later in the string, output everything up to that
-    if (code_s > 0): #{
-        f.close(); f = None;
-        print('Breaking at py tag');
-        return output + buf[:code_s], offset + code_s;
-    #}
-
-    # Starting with code tag
-    #offset = offset + code_e;
-    buf = buf[4:code_e].strip();
-    code_s = None;
-    #code_e = None;
-    read = 0;
-
-    print("Processing py tag:|" + buf + "|");
-    buf = eval(buf, {'title': title});
-
-    f.close(); f = None;
-    return buf, offset + code_e + 2;
+    return buf, read;
 #}
 
 # Returns:
@@ -90,7 +47,7 @@ def endGET(size_buf, offset, vardict, body): #{
 
     machine.reset();
 
-    return "HTTP/1.0 200 OK\nContent-Type: text/html\n\n", -1;
+    return '', -1;
 #}
 
 # vim:ts=4:tw=80:sw=4:et:ai:si
